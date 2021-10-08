29c7e1f9268e6c2949a7ca32479dee9ff62eb7ba
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2021-10-08 13:35:06 -0400

    Completed Lab 6

--- Lab 6/src/lab6/ArrayBasedStack.java
+++ Lab 6/src/lab6/ArrayBasedStack.java
@@ -1,87 +1,83 @@
 package lab6;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SuppressWarnings("hiding")
-public class ArrayBasedStack <E> implements Stack <E> {
+public class ArrayBasedStack<E> implements Stack<E> {
 
-	E[] stuff;  //internal storage for collections;
-	int size; 	//number of items;
+    E[] stuff; // internal storage for collections;
+    int size; // number of items;
 
-	//Constructor
-	public ArrayBasedStack()
-	{
-		stuff = (E[]) new Object[10]; // Use an array of size 10.
-		size = 0;	// The stack is initially empty.	
-			
-	} 
+    // Constructor
+    public ArrayBasedStack() {
+	stuff = (E[]) new Object[10]; // Use an array of size 10.
+	size = 0; // The stack is initially empty.
 
-	@Override
-	public void push(E x) {
-		//TODO
-		
+    }
+
+    @Override
+    public void push(E x) {
+	stuff[size] = x;
+	size++;
+
+    }
+
+    @Override
+    public void pop() {
+	stuff[size - 1] = null;
+	size--;
+
+    }
+
+    @Override
+    public E top() {
+	return stuff[size - 1];
+    }
+
+    @Override
+    public E topAndPop() {
+	E top = stuff[size - 1];
+	stuff[size - 1] = null;
+	size--;
+	return top;
+    }
+
+    @Override
+    public boolean isEmpty() {
+	return (size == 0);
+    }
+
+    @Override
+    public void makeEmpty() {
+	// TODO
+
+    }
+
+    public String toString() {
+	if (size == 0) {
+	    return null;
 	}
 
-	@Override
-	public void pop() {
-		//TODO
-
+	StringBuilder sb = new StringBuilder();
+	for (int i = 0; i < size; i++) {
+	    sb.append(stuff[i] + ", ");
 	}
 
-	@Override
-	public E top() {
-		return stuff[size-1];
-	}
+	sb.setLength(sb.length() - 2);
+	return sb.toString();
+    }
 
-	@Override
-	public E topAndPop() {
-		E top = stuff[size-1];
-		stuff[size-1] = null;
-		size--;
-		return top;
-	}
+    public static void main(String[] args) {
 
-	@Override
-	public boolean isEmpty() {
-		return (size == 0);
-	}
+	ArrayBasedStack<String> stack = new ArrayBasedStack<String>();
+	stack.push("hello");
+	stack.push("world");
 
+	System.out.println(stack);
 
-	@Override
-	public void makeEmpty() {
-		//TODO
-		
-		
-	}
+	stack.pop();
+	System.out.println(stack);
 
-	public String toString() {
-		if(size==0){
-			return null;
-		}
- 
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<size; i++){
-			sb.append(stuff[i] + ", ");
-		}
- 
-		sb.setLength(sb.length()-2);
-		return sb.toString();	
-	}
-	
-	public static void main(String[] args) {
+	stack.pop();
+	System.out.println(stack);
+    }
 
-		ArrayBasedStack<String> stack = new ArrayBasedStack<String>();
-		stack.push("hello");
-		stack.push("world");
-
-		System.out.println(stack);
-
-		stack.pop();
-		System.out.println(stack);
-
-		stack.pop();
-		System.out.println(stack);
-	}
-	
 }
--- Lab 6/src/lab6/ReverseString.java
+++ Lab 6/src/lab6/ReverseString.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
-        System.out.println("Please type any string to be reversed: ");
-    	Scanner scanner = new Scanner(System.in);
+	System.out.println("Please type any string to be reversed: ");
+	Scanner scanner = new Scanner(System.in);
 
-		//str variable references to the input string.
-        String str = scanner.nextLine();
-       
-		// The ArrayDeque class implements a more complete and consistent set of LIFO stack 
-		// operations, which should be used in preference to the Stack class.      
-    	// stack variable references to a new stack instance.
-    	ArrayDeque<Character> stack = new ArrayDeque<>();   
+	// str variable references to the input string.
+	String str = scanner.nextLine();
 
-		// Your code to reverse the input string, i.e. str, using the stack go at below:
+	// The ArrayDeque class implements a more complete and consistent set of LIFO
+	// stack
+	// operations, which should be used in preference to the Stack class.
+	// stack variable references to a new stack instance.
+	ArrayDeque<Character> stack = new ArrayDeque<>();
 
+	// Your code to reverse the input string, i.e. str, using the stack go at below:
+	for (int i = 0; i < str.length(); i++) {
+	    stack.push(str.charAt(i));
+	}
 
-
-
-
-
+	while (stack.size() > 0) {
+	    System.out.print(stack.pop());
+	}
 
     }
 }
