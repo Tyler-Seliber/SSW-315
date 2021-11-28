936562e514f7de719a3e204bac3841acfeab371f
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2021-11-27 21:38:59 -0500

    Working on part 2 step 4
    Able to hash all the files, but not sure why it seems to be accounting for duplicates for huck finn file (it shouldn't)

--- Lab 11/src/QuadraticProbingHashTableTest.java
+++ Lab 11/src/QuadraticProbingHashTableTest.java
@@ -1,5 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 import org.junit.jupiter.api.Test;
 
 class QuadraticProbingHashTableTest {
@@ -7,6 +11,10 @@ class QuadraticProbingHashTableTest {
     private MyInteger n1, n2, n3, n4, n5, n6, n7, n8, n9;
     private MyString s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
     private QuadraticProbingHashTable ht;
+    private final int DICTIONARY_SIZE = 45402;
+
+    private String dictionary_path, dictionary_rand_path, huck_finn_path;
+    private String[] dictionary, dictionary_rand, huck_finn;
 
     public QuadraticProbingHashTableTest() {
 	n1 = new MyInteger(7);
@@ -127,4 +135,107 @@ class QuadraticProbingHashTableTest {
 
     }
 
+    @Test
+    void testFiles() {
+	// String variables for the three text files' path names
+	dictionary_path = "linuxwords.txt";
+	dictionary_rand_path = "linuxwords_rand.txt";
+	huck_finn_path = "huckleberry_short.txt";
+
+	// String arrays to hold the contents of each text file
+	dictionary = fileReader(dictionary_path);
+	dictionary_rand = fileReader(dictionary_rand_path);
+	huck_finn = fileReader(huck_finn_path);
+
+	// Hash tables to hash the contents of each text file
+	QuadraticProbingHashTable dictionary_ht = new QuadraticProbingHashTable(DICTIONARY_SIZE * 2);
+	QuadraticProbingHashTable dictionary_rand_ht = new QuadraticProbingHashTable(DICTIONARY_SIZE * 2);
+	QuadraticProbingHashTable huck_finn_ht = new QuadraticProbingHashTable(DICTIONARY_SIZE * 2);
+
+	// Insert the contents of each text file to their respective hash tables
+	for (String s : dictionary) {
+	    MyString ms = new MyString(s);
+	    dictionary_ht.insert(ms);
+	}
+
+	for (String s : dictionary_rand) {
+	    MyString ms = new MyString(s);
+	    dictionary_rand_ht.insert(ms);
+	}
+
+	for (String s : huck_finn) {
+	    MyString ms = new MyString(s);
+	    huck_finn_ht.insert(ms);
+	}
+
+	// Check that each hash table has hashed all the words (duplicates NOT filtered)
+	HashEntry[] ht_arr;
+	int count;
+
+	ht_arr = dictionary_ht.getTable();
+	count = 0;
+	for (HashEntry h : ht_arr) {
+	    if (h == null)
+		continue;
+	    count++;
+	}
+
+	assertEquals(DICTIONARY_SIZE, count);
+
+	ht_arr = dictionary_rand_ht.getTable();
+	count = 0;
+	for (HashEntry h : ht_arr) {
+	    if (h == null)
+		continue;
+	    count++;
+	}
+
+	assertEquals(DICTIONARY_SIZE, count);
+
+	ht_arr = huck_finn_ht.getTable();
+	count = 0;
+	for (HashEntry h : ht_arr) {
+	    if (h == null)
+		continue;
+	    count++;
+	}
+
+//	assertEquals(DICTIONARY_SIZE, count);
+
+	// Integer variables to store the number of probes from each hash table
+	int dictionary_probes = dictionary_ht.getProbes();
+	int dictionary_rand_probes = dictionary_rand_ht.getProbes();
+	int huck_finn_probes = huck_finn_ht.getProbes();
+
+//	// Print the hash table
+//	ht_arr = huck_finn_ht.getTable();
+//	for (int i = 0; i < ht_arr.length; i++) {
+//	    HashEntry h = ht_arr[i];
+//	    if (h == null) {
+////		System.out.println("!NULL!");
+//		continue;
+//	    }
+//	    System.out.print(i + ":\t");
+//	    System.out.println(h.element);
+//	}
+
+    }
+
+    // Method to read the contents of a txt file
+    private String[] fileReader(String fileName) {
+	String[] contents = new String[DICTIONARY_SIZE];
+	try {
+	    File myObj = new File(fileName);
+	    Scanner myReader = new Scanner(myObj);
+	    for (int i = 0; myReader.hasNextLine(); i++) {
+		String data = myReader.nextLine();
+		contents[i] = data;
+	    }
+	    myReader.close();
+	} catch (FileNotFoundException e) {
+	    System.out.println("An error occurred.");
+	    e.printStackTrace();
+	}
+	return contents;
+    }
 }
